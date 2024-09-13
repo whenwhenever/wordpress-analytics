7b399071c206fec5090587ba54441868a6633056
WW <73618381+whenwhenever@users.noreply.github.com>
2024-09-13 16:03:57 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,9 @@
+/* -------- Mautic tracking pixel -------- */
+
+add_action('wp_head', 'mywebsite_analytics');
+function mywebsite_analytics() { ?>
+  
+<!--Paste your Google Analytics or Mautic code here-->
+  
+<?php
+}
